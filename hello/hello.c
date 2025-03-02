8b9762b8c652cc01a2e1c6bc0e54e58799d53890
Noah Wager <noahwager@gmail.com>
2025-03-01 22:44:45 -0800

    Use pr_* functions
    Signed-off-by: Noah Wager <noahwager@gmail.com>

--- hello/hello.c
+++ hello/hello.c
@@ -48,7 +48,7 @@ static struct file_operations cdev_fops = {
 static ssize_t sysfs_show(struct kobject *kobj, struct kobj_attribute *attr,
                           char *buf)
 {
-	printk(KERN_INFO LOG("sysfs read"));
+	pr_info(LOG("sysfs read"));
 	return sprintf(buf, "%d\n", val);
 }
 
@@ -58,7 +58,7 @@ static ssize_t sysfs_show(struct kobject *kobj, struct kobj_attribute *attr,
 static ssize_t sysfs_store(struct kobject *kobj, struct kobj_attribute *attr,
                            const char *buf, size_t count)
 {
-	printk(KERN_INFO LOG("sysfs write"));
+	pr_info(LOG("sysfs write"));
 	sscanf(buf, "%d", &val);
 	return count;
 }
@@ -68,7 +68,7 @@ static ssize_t sysfs_store(struct kobject *kobj, struct kobj_attribute *attr,
  */
 static ssize_t cdev_read(struct file *f, char __user *u, size_t l, loff_t *o)
 {
-	printk(KERN_INFO LOG("Read from character device"));
+	pr_info(LOG("Read from character device"));
 	return 0;
 }
 
@@ -76,25 +76,25 @@ static int __init driver_init(void)
 {
 	int error = 0;
 
-	printk(KERN_INFO LOG("Hello, kernel!"));
+	pr_info(LOG("Hello, kernel!"));
 
 	major = register_chrdev(0, CDEV_LABEL, &cdev_fops);
 	if (major < 0) {
-		printk(KERN_INFO LOG("Error registering cdev"));
+		pr_err(LOG("Error registering cdev"));
 		return major;
 	}
-	printk(KERN_INFO LOG("Major device number: %d"), major);
+	pr_info(LOG("Major device number: %d"), major);
 
 	// Add directory entry to /sys/kernel/
 	kobj_ref = kobject_create_and_add(SYSFS_ENTRY, kernel_kobj);
 	if (kobj_ref == NULL) {
-		printk(KERN_INFO LOG("Error creating sysfs entry"));
+		pr_err(LOG("Error creating sysfs entry"));
 		return -ENOMEM;
 	}
 	// Add file to the entry
 	error = sysfs_create_file(kobj_ref, &kobj_attr.attr);
 	if (error) {
-		printk(KERN_INFO LOG("Error creating sysfs file"));
+		pr_err(LOG("Error creating sysfs file"));
 		goto r_sysfs;
 	}
 
@@ -112,7 +112,7 @@ static void __exit driver_exit(void)
 	unregister_chrdev(major, CDEV_LABEL);
 	kobject_put(kobj_ref);
 	sysfs_remove_file(kernel_kobj, &kobj_attr.attr);
-	printk(KERN_INFO LOG("Goodbye, kernel!"));
+	pr_info(LOG("Goodbye, kernel!"));
 }
 
 module_init(driver_init);
